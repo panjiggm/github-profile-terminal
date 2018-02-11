d26172453fa105c7134555b8a6615098d048eac3
panjiggm <panjiggm@gmail.com>
2018-02-11 10:25:54 +0700

    Finish refactor to module profile

--- app.js
+++ app.js
@@ -1,30 +1,5 @@
-const https = require('https');
+const profile = require('./profile.js');
 
-const options = {
-    hostname: 'api.github.com',
-    port: 443,
-    path: '/users/panjiggm',
-    method: 'GET',
-    headers: {
-        'user-agent': 'nodejs'
-    }
-}
-// TODO: [x] Read the data
-let request = https.request(options, (response) => {
-    let body = '';
-    response.on('data', (data) => {
-        body = body + data;
-    })
-    response.on('end', () => {
-        // TODO: [x] Parse the data
-        // Convert String to JSON
-        let profile = JSON.parse(body)
-        // TODO: [x] Print the data out
-        console.log(`${profile.login} owns ${profile.public_repos} repo(s) and has ${profile.followers} follower(s)`);
-    })
-})
-
-request.end();
-request.on('error', (e) => {
-    console.log(e);
-})
+profile.get('panjiggm');
+profile.get('yofri');
+profile.get('rizafahmi');
---
+++ profile.js
@@ -0,0 +1,40 @@
+const https = require('https');
+
+let get = (username) => {
+    const options = {
+        hostname: 'api.github.com',
+        port: 443,
+        path: `/users/${username}`,
+        method: 'GET',
+        headers: {
+            'user-agent': 'nodejs'
+        }
+    }
+    // TODO: [x] Read the data
+    let request = https.request(options, (response) => {
+        let body = '';
+        response.on('data', (data) => {
+            body = body + data;
+        })
+        response.on('end', () => {
+            // TODO: [x] Parse the data
+            // Convert String to JSON
+            let profile = JSON.parse(body)
+            if (response.statusCode === 200){
+            // TODO: [x] Print the data out
+            console.log(`${profile.login} owns ${profile.public_repos} repo(s) and has ${profile.followers} follower(s)`);
+            } else {
+                console.log(`Profile with username '${username}' not found.`)
+            }
+        })
+    })
+    
+    request.end();
+    request.on('error', (e) => {
+        console.log(e);
+    })
+}
+
+module.exports = {
+    get
+};
